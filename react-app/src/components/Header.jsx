b0c9da9ea9782c99ee3f60eb035e39ecfa69215c
Weslei Ferreira Santos <wesleiferreira608@gmail.com>
2025-11-11 10:46:37 -0300

    feat: aprimorar cabeçalho com navegação responsiva e fechamento automático

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './Header.css';
 
 function Header() {
   const location = useLocation();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false);
+  };
 
   return (
-    <Navbar expand="lg" className="modern-navbar">
+    <Navbar expand="lg" className="modern-navbar" expanded={expanded} onToggle={setExpanded}>
       <Container fluid>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" onClick={handleNavClick}>
           <img
             src="/assets/img/logo.png"
             alt="Logo da Ferreira Tech Lab"
@@ -23,6 +29,7 @@ function Header() {
               as={Link} 
               to="/" 
               className={location.pathname === '/' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Sobre
             </Nav.Link>
@@ -30,6 +37,7 @@ function Header() {
               as={Link} 
               to="/symphony"
               className={location.pathname === '/symphony' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Symphony
             </Nav.Link>
@@ -37,6 +45,7 @@ function Header() {
               as={Link} 
               to="/horus"
               className={location.pathname === '/horus' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Horus
             </Nav.Link>
@@ -44,6 +53,7 @@ function Header() {
               as={Link} 
               to="/projects"
               className={location.pathname === '/projects' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Projetos
             </Nav.Link>
@@ -51,6 +61,7 @@ function Header() {
               as={Link} 
               to="/donates"
               className={location.pathname === '/donates' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Doações
             </Nav.Link>
@@ -58,6 +69,7 @@ function Header() {
               as={Link} 
               to="/contact"
               className={location.pathname === '/contact' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Contato
             </Nav.Link>
@@ -65,6 +77,7 @@ function Header() {
               as={Link} 
               to="/rules"
               className={location.pathname === '/rules' ? 'active' : ''}
+              onClick={handleNavClick}
             >
               Envie seu App
             </Nav.Link>
